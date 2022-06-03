68eec9b64770fc471e2bfa1b1964aa8befe30eb0
zahid-rahman <rahmanzahid94@gmail.com>
2022-06-03 19:27:21 +0600

    WIP:added test cases for complain api

--- api/controllers/complainController.js
+++ api/controllers/complainController.js
@@ -1,43 +1,117 @@
 const httpStatus = require("http-status")
+const complainModel = require('./../models').complain;
 
 const viewAllComplain = (req, res) => {
-    return res.status(httpStatus.OK).json({
-        message: "view all complain",
-        data: null,
-        responseCode: httpStatus.OK
-    });    
-}
-
-const createComplain = (req, res) => {
-    return res.status(httpStatus.CREATED).json({
-        message: "create complain",
-        data: null,
-        responseCode: httpStatus.OK
-    });   
-}
-
-const viewComplain = (req, res) => {
-    return res.status(httpStatus.OK).json({
-        message: "view a complain",
-        data: null,
-        responseCode: httpStatus.OK
-    });   
-}
-
-const deleteComplain = (req, res) => {
-    return res.status(httpStatus.OK).json({
-        message: "delet a complain",
-        data: null,
-        responseCode: httpStatus.OK
+    complainModel.findAll()
+    .then((response) => {
+        return res.status(httpStatus.OK).json({
+            message: "view all complain",
+            data: response,
+            responseCode: httpStatus.OK
+        });
+    })
+    .catch((error) => {
+        console.error(error)
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: "Something went wrong",
+            data: error,
+            responseCode: httpStatus.BAD_REQUEST
+        });
     });
 }
 
+const createComplain = (req, res) => {
+    complainModel.create(req.body)
+    .then((response) => {
+        return res.status(httpStatus.CREATED).json({
+            message: "complain created successfully",
+            data: response,
+            responseCode: httpStatus.CREATED
+        }); 
+    })
+    .catch((error) => {
+        console.error(error)
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: "Something went wrong",
+            data: error,
+            responseCode: httpStatus.BAD_REQUEST
+        });
+    }) 
+}
+
+const viewComplain = (req, res) => {
+    const { id } = req.params;
+
+    complainModel.findOne({
+        where: {
+            id
+        }
+    })
+    .then((response) => {
+        return res.status(httpStatus.OK).json({
+            message: "view a complain",
+            data: response,
+            responseCode: httpStatus.OK
+        });  
+    })
+    .catch((error) => {
+        console.error(error)
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: "Something went wrong",
+            data: error,
+            responseCode: httpStatus.BAD_REQUEST
+        });
+    }) 
+}
+
+const deleteComplain = (req, res) => {
+    const { id } = req.params;
+    complainModel.destroy({
+        where: {
+            id
+        },
+        returning: true
+    })
+    .then((response) => {
+        return res.status(httpStatus.OK).json({
+            message: "delet a complain",
+            data: response,
+            responseCode: httpStatus.OK
+        });
+    })
+    .catch((error) => {
+        console.error(error)
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: "Something went wrong",
+            data: error,
+            responseCode: httpStatus.BAD_REQUEST
+        });
+    })
+}
+
 const editComplain = (req, res) => {
-    return res.status(httpStatus.OK).json({
-        message: "edit a complain",
-        data: null,
-        responseCode: httpStatus.OK
-    });  
+    const { id } = req.params;
+    complainModel.update(req.body,{
+        where: {
+            id
+        },
+        returning: true,
+    })
+    .then((response) => {
+        return res.status(httpStatus.OK).json({
+            message: "edit a complain",
+            data: response[1][0],
+            responseCode: httpStatus.OK
+        }); 
+    })
+    .catch((error) => {
+        console.error(error)
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: "Something went wrong",
+            data: error,
+            responseCode: httpStatus.BAD_REQUEST
+        });
+    })
 }
 
 module.exports = {
--- api/test/complain.test.js
+++ api/test/complain.test.js
@@ -4,17 +4,13 @@ const app = require('../app');
 const { Sequelize } = require('sequelize');
 const { log } = require('winston');
 let token = null;
-
-
-
-
-// const sequelize = new Sequelize(`postgres://db_user:alsdkfjl;eilaksfd90asf03:5432/complain_db`)
+let latestComplainId = null;
 
 const sequelize = new Sequelize('complain_test_db', 'db_test_user', 'test', {
     host: 'localhost',
     dialect: 'postgres',
     port: '5433'
-  });
+});
 
 
 const emailName = (Math.random() + 1).toString(36).substring(7);
@@ -35,8 +31,8 @@ describe("ALL complain api test suite", () => {
     beforeAll(async () => {
         await sequelize.authenticate();
     })
-    it('create user', (done) => {
-            request(app)
+    it('POST /api/user/register', (done) => {
+        request(app)
             .post('/api/user/register')
             .send(createUserPayload)
             .expect('Content-Type', /json/)
@@ -53,23 +49,111 @@ describe("ALL complain api test suite", () => {
             })
     });
 
-    it('login user', (done) => {
+    it('POST /api/user/login', (done) => {
         request(app).post('/api/user/login')
-        .send(loginUserPayload)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end((error, response) => {
-            token = response.body.data
-            console.log(response.body)
-            expect(response.body).toEqual(expect.objectContaining({
-                data: token,
-                responseCode: 200
-            }));
-            done();
-        })
+            .send(loginUserPayload)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((error, response) => {
+                token = response.body.data;
+                console.log(token)
+                expect(response.body).toEqual(expect.objectContaining({
+                    data: token,
+                    responseCode: 200
+                }));
+                done();
+            })
     });
 
-    afterAll(async() => {
+    it('POST /api/complain/create', (done) => {
+        request(app)
+            .post('/api/complain/create')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                name: 'insecurity at night',
+                region: 'Malibag, dhaka',
+                description: 'it is not safe to hangout at night time in malibag area specially with a girl. It is too dangerous',
+                longitude: '24.534',
+                lattitude: '23.2343',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            })
+            .expect(201)
+            .end((error, response) => {
+                latestComplainId = response.body.data.id
+                expect(response.body.responseCode).toEqual(201);
+                done();
+            });
+    });
+
+    it('GET /api/complain/view-all', (done) => {
+        request(app)
+            .get('/api/complain/view-all')
+            .set('Authorization', `Bearer ${token}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((error, response) => {
+                // expect(response.body).toBe(expect.objectContaining({
+                //     message: "view all complain",
+                //     data: expect.arrayContaining([
+                //         expect.objectContaining({
+                //             id: 1,
+                //             name: expect.any(String),
+                //             region: expect.any(String),
+                //             description: expect.any(String),
+                //             longitude: expect.any(String),
+                //             lattitude: expect.any(String),
+                //             createdAt: expect.any(Date),
+                //             updatedAt: expect.any(Date),
+                //         })
+                //     ]),
+                //     responseCode: 200
+                // }))
+
+                expect(response.body.responseCode).toEqual(200);
+                done();
+            })
+    });
+
+    it('GET /api/complain/view/:id', (done) => {
+        request(app)
+            .get('/api/complain/view/1')
+            .set('Authorization', `Bearer ${token}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((error, response) => {
+                expect(response.body.responseCode).toEqual(200);
+                expect(response.body.data.id).toEqual(1)
+                done();
+            })
+    });
+
+    it('PATCH /api/complain/edit/:id', (done) => {
+        request(app)
+            .patch('/api/complain/edit/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send({description: "demo description changed"})
+            .expect(200)
+            .end((error, response) => {
+                expect(response.body.responseCode).toEqual(200);
+                expect(response.body.data.description).toEqual("demo description changed")
+                done();
+            })
+    });
+
+    it('DELETE /api/complain/delete/:id', (done) => {
+        request(app)
+            .delete(`/api/complain/delete/${latestComplainId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+            .end((error, response) => {
+                expect(response.body.responseCode).toEqual(200);
+                expect(response.body.data).toEqual(1)
+                done();
+            })
+    })
+
+    afterAll(async () => {
         await sequelize.close()
     })
 })
